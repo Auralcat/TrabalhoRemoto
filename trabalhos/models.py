199f205ef2c6376984e6a5a644979505de31b4e2
Auralcat <ashleyzt@hi2.in>
2018-10-10 12:57:06 -0300

    Criar modelo Vagas

--- trabalhos/models.py
+++ trabalhos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Vaga(models.Model):
+    titulo    = models.CharField(max_length=200)
+    descricao = models.TextField()
+    salario   = models.DecimalField(max_digits=7,  decimal_places=2, null=False, blank=False)
